90e8a0549022221f547eb9ef6a1b10d87ba29d9f
Rahmanberdi <rahmanberdihojanazarow@gmail.com>
2025-11-07 21:53:11 +0800

    engines field added

--- backend/src/server.js
+++ backend/src/server.js
@@ -23,13 +23,13 @@ app.use('/api/messages',messageRoute);
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    app.get('*',(req,res)=>{
+    app.get('*',(_,res)=>{
        res.sendFile(path.join(__dirname, "../frontend","dist","index.html"));
     });
 }
 
 
 app.listen(PORT,()=>{ 
-    console.log(`Server running on  https://localhost:${PORT}`);
+    console.log(`Server running on  http://localhost:${PORT}`);
     
 });

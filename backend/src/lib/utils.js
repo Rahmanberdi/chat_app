3afe9490755aae3d678c9807189ad89ed680da21
Rahmanberdi <rahmanberdihojanazarow@gmail.com>
2025-11-08 17:57:13 +0800

    signup section code refactoring

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,13 +1,17 @@
 import jwt from 'jsonwebtoken';
 export const generateToken = (userId,res) => {
-     const token = jwt.sign({userId},process.env.JWT_SECRET,{
-         expiresIn: '7d',
-     });
+     const {JWT_SECRET} = process.env;
+     if (!JWT_SECRET) {
+         throw new Error("Missing JWT_SECRET");
+     }
+     const token = jwt.sign({userId},JWT_SECRET,{
+       expiresIn: "7d"
+    });
      res.cookie('jwt',token,{
          maxAge:7*24*60*60*1000,  //MS
          httpOnly:true, //prevent XSS attacks: cross-site scripting
          sameSite: 'strict', //CSRF attacks
-         secure:process.env.NODE_ENV === "development" ? false : true,
+         secure:process.env.NODE_ENV !== "development",
      });
      return token;
 }
